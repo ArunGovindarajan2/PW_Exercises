2403cc0b3990149529546a1606ac21ccb563fc86
ArunGovindarajan2 <arung.tnj@gmail.com>
2026-09-02 18:26:22 +0530

    Add files via upload

---
+++ Exercise_001.js
@@ -0,0 +1,25 @@
+// Variable decalation - 'let'
+
+let firstName='Arun'
+console.log(firstName)
+console.log(typeof firstName)
+
+let companyName='ABC Technologies'
+console.log(companyName)
+console.log(typeof companyName)
+
+let mobileNumber=9751330722
+console.log(mobileNumber)
+console.log(typeof mobileNumber)
+
+let isAutomation=true
+console.log(isAutomation)
+console.log(typeof isAutomation)
+
+let hasPlaywright
+console.log(hasPlaywright)
+console.log(typeof hasPlaywright)
+
+
+
+
